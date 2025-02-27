23ec05170d29ab484054a2444cce133238b33c64
Pakkapong2 <pmiswnnil@gmail.com>
2025-02-27 13:46:38 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const carRoute = require("./routes/car.route");
