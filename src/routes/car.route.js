ba719d86311100cbfdfb694f5d3607d62057fab8
Pakkapong2 <pmiswnnil@gmail.com>
2025-02-28 08:06:04 +0700

    update

--- src/routes/car.route.js
+++ src/routes/car.route.js
@@ -5,8 +5,8 @@ const controller = require("../controllers/car.controller");
 // define routes here
 app.get("/", controller.getAllCars);
 app.get("/:id", controller.getCarById);
-// app.post("/", controller.create);
-// app.put("/:id", controller.put);
-// app.patch("/:id", controller.patch);
-// app.delete("/:id", controller.delete);
+app.post("/", controller.create);
+app.put("/:id", controller.put);
+app.patch("/:id", controller.patch);
+app.delete("/:id", controller.delete);
 module.exports = app;
